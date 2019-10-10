69f77581360595cb391718c0559aa898e55be8e8
Olimpia <olimpiacompean@prusuit.org>
2019-10-10 18:24:21 -0400

    added a label to the detailed view

--- REVIEW-UI/DetailedView.swift
+++ REVIEW-UI/DetailedView.swift
@@ -7,6 +7,11 @@ class DetailedView: UIView {
         return image
     }()
     
+    lazy var recepieName: UILabel = {
+        let name = UILabel()
+        return name
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: UIScreen.main.bounds)
         commonInit()
@@ -14,18 +19,21 @@ class DetailedView: UIView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-        commonInit()
+       
     }
     
     private func commonInit() {
         addSubview(recepieImage)
+        addSubview(recepieName)
         setContrains()
     }
     
     func setContrains() {
         recepieImage.translatesAutoresizingMaskIntoConstraints = false
         
-        [recepieImage.centerYAnchor.constraint(equalTo: safeAreaLayoutGuide.centerYAnchor), recepieImage.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor)].forEach{ $0.isActive = true }
+//        [recepieImage.centerYAnchor.constraint(equalTo: safeAreaLayoutGuide.centerYAnchor), recepieImage.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor)].forEach{ $0.isActive = true }
+        
+        [recepieImage.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 22), recepieImage.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 44), recepieImage.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -44), recepieImage.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -80)].forEach{ $0.isActive = true}
     }
     
     
